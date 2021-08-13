1151ae64b5a8a294bb0bceac7e88cc694e7f532d
Peter Lucia <peterlucia@gatech.edu>
2021-08-12 23:09:26 -0700

    Added add_binary.rs

---
+++ src/add_binary.rs
@@ -0,0 +1,42 @@
+
+/*
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+ */
+pub struct Solution {
+
+}
+
+impl Solution {
+    pub fn binary_string_to_i32(num: String) -> i32 {
+        return i32::from_str_radix(&*num, 2).unwrap();
+    }
+
+    pub fn i32_to_binary_string(num: i32) -> String {
+        return format!("{:b}", num);
+    }
+
+    pub fn add_binary(&self, a: String, b: String) -> String {
+        /*
+        &self     - just want to read the data in the struct
+        &mut self - want to change the instance we've called the method on
+        self      - the method transforms self into something else
+                    and you want to prevent the caller from using the original
+                    instance after the transformation.
+
+         */
+        let mut num1 = Solution::binary_string_to_i32(a);
+        let mut num2 = Solution::binary_string_to_i32(b);
+        let mut total = num1 + num2;
+        println!("{0} + {1} = {2}", num1, num2, total);
+        return Solution::i32_to_binary_string(total);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // declare remove_element as a module
 mod remove_element;
 mod two_sum;
+mod add_binary;
 
 fn main() {
     if false {
@@ -15,10 +16,20 @@ fn main() {
         println!("values = {0:?}", nums);
     }
 
-    if true {
+    if false {
         let nums = vec![2, 7, 11, 15];
         let result = two_sum::two_sum(nums, 9);
         println!("Indices adding up to 9 = {0:?}", result);
+    }
+
+    if true {
+        let solution = add_binary::Solution{};
+        assert_eq!(String::from("100"),
+                   solution.add_binary(String::from("11"),
+                                          String::from("1")));
+        assert_eq!(String::from("10101"),
+                   solution.add_binary(String::from("1010"),
+                                          String::from("1011")));
     }
 }
 
