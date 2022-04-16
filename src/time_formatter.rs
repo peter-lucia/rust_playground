30e6ba942d9ca05412c34b5a385e7d1c6df8b918
Peter Lucia <plucia19@gmail.com>
2022-04-15 23:11:18 -0500

    Updated datetime conversion

--- src/time_formatter.rs
+++ src/time_formatter.rs
@@ -2,6 +2,8 @@ use chrono::{DateTime, TimeZone, NaiveDateTime, Utc, Offset, Local};
 use chrono::format::strftime;
 use std::borrow::Borrow;
 use std::thread::current;
+use chrono_tz::Tz;
+use chrono_tz::UTC;
 
 pub struct TimeFormatter{
 
@@ -11,41 +13,25 @@ impl TimeFormatter {
 
 
     /*
-    Converts an isoformatted, timezone aware datetime string to local time
-    Example:
-        Input: "2022-03-30T05:18:14.660921+00:00";
-        Output: "2022-03-29T22:18:14.660921-07:00";
+    Converts an rc3339 formatted utc datetime string to the desired timezone
 
     Sources:
         * https://docs.rs/chrono/0.4.11/chrono/struct.DateTime.html
         * https://docs.rs/chrono/latest/chrono/format/strftime/index.html
         * https://docs.rs/chrono/latest/chrono/
+        * https://docs.rs/chrono-tz/latest/chrono_tz/
      */
-    pub fn convert_to_local(&self, utc_time_with_offset: &str) -> String {
+    pub fn convert(&self, utc_time: &str, desired_timezone: &str) -> Option<String> {
 
-        let current_time = Local::now();
-        let tz = current_time.timezone();
-        let local_timezone = current_time.format("%Z").to_string();
-        println!("Detected local timezone: {0}", local_timezone);
-
+        let tz: Tz = desired_timezone.parse().unwrap();
         let mut parsed =
-            DateTime::parse_from_rfc3339(utc_time_with_offset);
-
+            DateTime::parse_from_rfc3339(utc_time);
 
         if parsed.is_ok() {
-            return parsed.unwrap().with_timezone(&tz).to_rfc3339();
+            return Some(parsed.unwrap().with_timezone(&UTC).with_timezone(&tz).to_rfc3339());
         }
 
-        println!("Trying backup parsing method...");
-
-        let fmt = "%+";
-        parsed = DateTime::parse_from_str(
-            utc_time_with_offset,
-            fmt
-        );
-
-        return parsed.unwrap().with_timezone(&tz).to_rfc3339();
-
+        return None;
     }
 }
 
@@ -58,7 +44,7 @@ mod tests {
         let t_formatter = TimeFormatter{};
         let input_time_utc= "2022-03-30T05:18:14.660921+00:00";
         let expected_result = "2022-03-29T22:18:14.660921-07:00"; // only works in daylight savings time
-        let actual_result = t_formatter.convert_to_local(input_time_utc);
-        assert_eq!(expected_result, actual_result);
+        let actual_result = t_formatter.convert(input_time_utc, "US/Pacific");
+        assert_eq!(expected_result, actual_result.unwrap());
     }
 }
